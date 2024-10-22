1c771fb4038d6389b35b2b3947c2865307e84d8a
Mihir <hello@mihirkavishwar.com>
2024-10-21 18:11:47 -0700

    Idea class creation

--- Idea.py
+++ Idea.py
@@ -1,4 +1,7 @@
 class Idea:
-    def __init__(self):
+    def __init__(self, name, *args, **kwargs):
         print('Idea Object created.')
-        self.name = ''
+        self.name = name  # First argument is mandatory and assigned to 'name'
+        self.description = args[0] if args else None  # Optional second argument
+        self.kwargs = kwargs
+
